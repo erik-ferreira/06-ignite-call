3646c5c0494a4f5ef664b83f63789ee3818b87d9
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-24 15:52:17 -0300

    create visual structure home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+import { Heading, Text } from "@ignite-ui/react"
+
+import previewImage from "../../assets/app-preview.png"
+
+import { Container, Hero, Preview } from "./styles"
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando aplicação em funcionamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
