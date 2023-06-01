7ebae2eab1bcbd0f764b5971af6f5930ed820467
erik-ferreira <erikdesouzagf@gmail.com>
2023-06-01 14:58:15 -0300

    styling schedule

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -2,7 +2,7 @@ interface GetWeekDaysProps {
   short?: boolean
 }
 
-export function getWeekDays({ short = false }: GetWeekDaysProps) {
+export function getWeekDays({ short = false }: GetWeekDaysProps = {}) {
   const formatter = new Intl.DateTimeFormat("pt-BR", { weekday: "long" })
 
   // const test = .map(day => formatter.format())
