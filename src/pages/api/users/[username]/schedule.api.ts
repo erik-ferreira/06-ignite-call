09f5c53b308f576e4e376a7d9525a012bd8b5f0e
erik-ferreira <erikdesouzagf@gmail.com>
2023-06-06 16:02:37 -0300

    create scheduling in calendar user

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,8 +1,10 @@
 import { z } from "zod"
 import dayjs from "dayjs"
+import { google } from "googleapis"
 import { NextApiRequest, NextApiResponse } from "next"
 
 import { prisma } from "../../../../lib/prisma"
+import { getGoogleOAuthToken } from "../../../../lib/google"
 
 const createSChedulingBodySchema = z.object({
   name: z.string(),
@@ -42,26 +44,55 @@ export default async function handler(
     return res.status(400).json({ message: "Date is in the past." })
   }
 
-  const conflitingScheduling = await prisma.scheduling.findFirst({
+  const conflictingScheduling = await prisma.scheduling.findFirst({
     where: {
       user_id: user.id,
       date: schedulingDate.toDate(),
     },
   })
 
-  if (conflitingScheduling) {
+  if (conflictingScheduling) {
     return res
       .status(400)
       .json({ message: "There is another scheduling at the same time." })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: "v3",
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: "primary",
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, "hour").format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: "hangoutsMeet",
+          },
+        },
+      },
     },
   })
 
